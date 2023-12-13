6370e4d28f9fe8945ed117cccfc745f7cfeca0c2
doublekari <kaykayyaw@gmail.com>
2023-12-13 23:58:25 +0000

    start PersonalInfo page

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { NavLink, Outlet } from "react-router-dom"
+import { NavLink, Outlet, Link } from "react-router-dom"
 const RootLayout = () => {
   const navItems = [
     {
@@ -24,43 +24,48 @@ const RootLayout = () => {
     }
   ]
   return (
-    <>
-    <header className="md:col-span-1 md:h-screen font-ubuntu md:px-10 md:py-8">
-        <nav className="bg-sidebar-mobile md:bg-sidebar-desktop bg-center bg-no-repeat bg-cover h-40  pt-8 md:h-full md:rounded-xl">
-            <ul className="flex justify-center gap-x-4  md:flex-col md:gap-y-8 md:pl-5">
-                {
-                    navItems.map(item=> {
-                        return (
-                       
-                    <li key={item.step}  className="flex items-center gap-x-4">
-                        <NavLink to={item.path} 
-                        style={({isActive})=>{
-                            return {
-                                color: isActive ? 'hsl(213, 96%, 18%)': 'hsl(0, 0%, 100%)',
-                                background: isActive? 'hsl(228, 100%, 84%)': "inherit",
-                                borderRadius: "100%"
+    <div className="md:w-[800px] min-h-screen   mx-auto md:grid md:grid-cols-3 ">
+        <header className="md:col-span-1  font-ubuntu md:px-10 md:py-8 bg-neutral-alabaster">
+            <nav className="bg-sidebar-mobile md:bg-sidebar-desktop bg-center bg-no-repeat bg-cover h-40  pt-8 md:h-full md:rounded-xl">
+                <ul className="flex justify-center gap-x-4  md:flex-col md:gap-y-8 md:pl-5">
+                    {
+                        navItems.map(item=> {
+                            return (
+                        
+                        <li key={item.step}  className="flex items-center gap-x-4">
+                            <NavLink to={item.path} 
+                            style={({isActive})=>{
+                                return {
+                                    color: isActive ? 'hsl(213, 96%, 18%)': 'hsl(0, 0%, 100%)',
+                                    background: isActive? 'hsl(228, 100%, 84%)': "inherit",
+                                    borderRadius: "100%"
 
-                            }
-                        }}   
-                        className="flex justify-center items-center border border-neutral-White rounded-full w-8 h-8 md:w-10 md:h-10">
-                                <h2 className="font-bold">{item.step}</h2>
-                        </NavLink>
-                        <div className="hidden md:block text-neutral-White">
-                                    <p className="uppercase text-xs text-neutral-light-gray tracking-wider">step {item.step}</p>
-                                    <h3 className="uppercase font-bold text-sm tracking-wider">{item.title}</h3>
-                        </div>      
-                      
-                    </li>
-                        )
-                    })
-                }
-            </ul>
-        </nav>
-    </header>
-    <main className="col-span-3 font-ubuntu md:pr-10 md:py-8">
-        <Outlet/>
-    </main>
-    </>
+                                }
+                            }}   
+                            className="flex justify-center items-center border border-neutral-White rounded-full w-8 h-8 md:w-10 md:h-10">
+                                    <h2 className="font-bold">{item.step}</h2>
+                            </NavLink>
+                            <div className="hidden md:block text-neutral-White">
+                                        <p className="uppercase text-xs text-neutral-light-gray tracking-wider">step {item.step}</p>
+                                        <h3 className="uppercase font-bold text-sm tracking-wider">{item.title}</h3>
+                            </div>      
+                        
+                        </li>
+                            )
+                        })
+                    }
+                </ul>
+            </nav>
+        </header>
+        <main className="md:col-span-2 w-[340px] md:w-full mx-auto -mt-16 shadow-xl rounded-xl flex flex-col items-center pt-7 pb-8 px-5 gap-y-5 bg-neutral-alabaster ">
+            <Outlet/>
+        </main>
+        <footer className="absolute bottom-0 left-0 right-0 bg-neutral-alabaster py-4 px-4 ">
+            <Link>
+                <button className="py-3 px-6 rounded-md text-neutral-magnolia bg-primary-marine-blue float-right">Next Step</button>
+            </Link>
+        </footer>
+    </div>
   )
 }
 
--- src/pages/PersonalInfo.jsx
+++ src/pages/PersonalInfo.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
-
 const PersonalInfo = () => {
   return (
-    <div>PersonalInfo</div>
+  
+      <section className="px-3 space-y-4">
+        <div className="space-y-3">
+          <h2 className="text-3xl text-primary-marine-blue font-bold">Personal Info</h2>
+          <p className="text-neutral-cool-gray">Please provide your name, email address, and phone number.</p>
+        </div>
+
+      <form action="#" className="space-y-5 text-primary-marine-blue">
+        <div>
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" className="block w-full border text-neutral-cool-gray py-3 px-4 rounded-md  outline-none" placeholder="e.g Stephen King" />
+        </div>
+        <div>
+          <label htmlFor="mail">Email Address</label>
+          <input type="email" id="mail" className="block w-full border text-neutral-cool-gray  py-3 px-4 rounded-md outline-none" placeholder="e.g. stephenking@lorem.com" />
+        </div>
+        <div>
+          <label htmlFor="contact">Phone Number</label>
+          <input type="phone" id="contact" className="block w-full border text-neutral-cool-gray py-3 px-4 rounded-md outline-none" placeholder="e.g. +1 234 567 890" />
+        </div>
+      </form>
+       
+      </section>
+   
   )
 }
 
