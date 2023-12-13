6f12485c4d2c8b2f8ba632ebafb0d2f3f01ca93b
doublekari <kaykayyaw@gmail.com>
2023-12-13 23:54:34 +0000

    fix:responsive issue of summary page

--- src/pages/Summary.jsx
+++ src/pages/Summary.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import thankyou from "../../public/images/icon-thank-you.svg"
 
 const Summary = () => {
   return (
-    <div>Summary</div>
+  
+      <>
+        <picture className="pt-10"><img src={thankyou} alt="A check" /></picture>
+        <div className="text-center">
+          <h1 className="text-primary-marine-blue text-3xl font-black pb-2">Thank you!</h1>
+          <p className="text-neutral-cool-gray">Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support,
+            please feel free to email us at support@loremgaming.com
+          </p>
+        </div>
+      </>
+   
   )
 }
 
